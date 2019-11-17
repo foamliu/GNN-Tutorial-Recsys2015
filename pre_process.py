f28381ec7a7cf71bd8f8b6e3c5c57a92e76f9695
unknown <foamliu@yeah.net>
2019-11-17 16:25:02 +0800

    update

--- pre_process.py
+++ pre_process.py
@@ -32,3 +32,4 @@ if __name__ == '__main__':
     processed_path = 'data/yoochoose_click_binary_1M_sess.dataset'
     with open(processed_path, 'wb') as f:
         torch.save(clicks, f)
+    logger.info('Completed!')
